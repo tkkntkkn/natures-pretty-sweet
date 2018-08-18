59c5fe1150965b0174464b46c1468425f2e77df8
tkkntkkn <knordsiek@gmail.com>
2018-08-18 13:41:21 -0400

    starting to add wildflower/superbloom events

---
+++ Source/NPS/NPS/BiomeSeasonalSettings.cs
@@ -0,0 +1,13 @@
+﻿using Verse;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+
+namespace TKKN_NPS
+{
+	class BiomeSeasonalSettings : DefModExtension
+	{
+		public List<ThingDef> bloomPlants;
+		public List<Season> bloomSeasons;
+	}
+}
