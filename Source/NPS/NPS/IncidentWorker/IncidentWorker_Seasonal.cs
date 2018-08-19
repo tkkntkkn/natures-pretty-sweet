6a9e77dc2a01df433c8378ce3dfa663a0df0e848
tkkntkkn <knordsiek@gmail.com>
2018-08-18 22:35:48 -0400

    more tweaks to keep blooms in the right area

--- Source/NPS/NPS/IncidentWorker/IncidentWorker_Seasonal.cs
+++ Source/NPS/NPS/IncidentWorker/IncidentWorker_Seasonal.cs
@@ -1,56 +1,85 @@
-﻿using System;
-using RimWorld;
+﻿using RimWorld;
 using Verse;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace TKKN_NPS
 {
 
-	public class IncidentWorker_WildFlowerBloom : IncidentWorker
+	public class IncidentWorker_WildFlowerBloom : IncidentWorker_Bloom
 	{
+		public string label = "TKKN_NPS_WildFlowerBloom".Translate();
+		public string text = "TKKN_NPS_WildFlowerBloomTxt".Translate();
 		public ThingDef thingDef;
-
-		protected override bool TryExecuteWorker(IncidentParms parms)
-		{
-			Map map = (Map)parms.target;
-			IntVec3 intVec = CellFinder.RandomNotEdgeCell(15, map);
-
-			if (!Settings.allowPlantEffects)
-			{
-				return false;
-			}
-
-			string label = "TKKN_NPS_WildFlowerBloom".Translate();
-			string text = "TKKN_NPS_WildFlowerBloomTxt".Translate();
-
-			Find.LetterStack.ReceiveLetter(label, text, LetterDefOf.NeutralEvent);
-
-
-			return true;
-		}
+		
 	}
 }
 
 namespace TKKN_NPS
 {
 
-	public class IncidentWorker_Superbloom : IncidentWorker
+	public class IncidentWorker_Superbloom : IncidentWorker_Bloom
 	{
+
+		public string label = "TKKN_NPS_Superbloom".Translate();
+		public string text = "TKKN_NPS_SuperbloomTxt".Translate();
+		public ThingDef thingDef;
+
+	
+	}
+}
+
+namespace TKKN_NPS
+{
+
+	public class IncidentWorker_Bloom : IncidentWorker
+	{
+		public string label;
+		public string text;
 		public ThingDef thingDef;
 
 		protected override bool TryExecuteWorker(IncidentParms parms)
 		{
-			Map map = (Map)parms.target;
-			IntVec3 intVec = CellFinder.RandomNotEdgeCell(15, map);
 
 			if (!Settings.allowPlantEffects)
 			{
 				return false;
 			}
 
-			string label = "TKKN_NPS_Superbloom".Translate();
-			string text = "TKKN_NPS_SuperbloomTxt".Translate();
+			Map map = (Map)parms.target;
+			IntVec3 intVec = CellFinder.RandomNotEdgeCell(15, map);
 
-			Find.LetterStack.ReceiveLetter(label, text, LetterDefOf.NeutralEvent);
+			//can the biome support it?
+			bool canBloomHere = true;
+			BiomeSeasonalSettings biomeSettings = map.Biome.GetModExtension<BiomeSeasonalSettings>();
+			List<ThingDef> bloomPlants = biomeSettings.bloomPlants.ToList<ThingDef>();
+			if (bloomPlants.Count == 0)
+			{
+				return false;
+			}
+			List<Season> allowedSeasons = biomeSettings.bloomSeasons.ToList<Season>();
+			Vector2 location = Find.WorldGrid.LongLatOf(Find.VisibleMap.Tile);
+			Season season = GenDate.Season((long)Find.TickManager.TicksAbs, location);
+
+			for (int j = 0; j < allowedSeasons.Count; j++)
+			{
+
+				if (season != allowedSeasons[j])
+				{
+					canBloomHere = false;
+					break;
+				}
+			}
+
+			if (!canBloomHere)
+			{
+				return false;
+			}
+
+
+
+			Find.LetterStack.ReceiveLetter(this.label, this.text, LetterDefOf.NeutralEvent);
 
 
 			return true;
