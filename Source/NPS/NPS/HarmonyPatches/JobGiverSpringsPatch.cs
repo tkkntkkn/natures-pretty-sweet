14765132707d35c2e08e367af09c0236af59ecbf
tkkntkkn <knordsiek@gmail.com>
2018-08-18 22:20:25 -0400

    started adding relax/cool down jobs

---
+++ Source/NPS/NPS/HarmonyPatches/JobGiverSpringsPatch.cs
@@ -0,0 +1,59 @@
+﻿using RimWorld;
+using Harmony;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+
+namespace TKKN_NPS
+{
+	//pawns will go sit in cold springs to cool off if there is no better option
+	[HarmonyPatch(typeof(JobGiver_SeekSafeTemperature))]
+	[HarmonyPatch("TryGiveJob")]
+	class PatchTryGiveJob
+	{
+		public static void Postfix(ref Job __result, Pawn pawn)
+		{
+			if (__result == null) {
+				if (Find.VisibleMap.GetComponent<Watcher>().activeSprings.Count != 0)
+				{
+					__result = null;
+					return;
+				}
+
+				bool isHot = false;
+				for (int i = 0; i < pawn.health.hediffSet.hediffs.Count; i++)
+				{
+					if (pawn.health.hediffSet.hediffs[i].def == RimWorld.HediffDefOf.Heatstroke && pawn.health.hediffSet.hediffs[i].CurStageIndex >= (int)TemperatureInjuryStage.Serious)
+					{
+						isHot = true;
+						break;
+					}
+				}
+
+				if (!isHot)
+				{
+					__result = null;
+					return;
+				}
+
+				TerrainDef terrain = pawn.Position.GetTerrain(Find.VisibleMap);
+				if (isHot && terrain.defName == "TKKN_ColdSpringsWater") 
+				{
+					__result = new Job(JobDefOf.WaitSafeTemperature, 500, true);
+					return;
+				}
+
+				//send them to closest spring to relax
+
+				Thing thing = GenClosest.ClosestThingReachable(pawn.GetLord().CurLordToil.FlagLoc, pawn.Map, ThingRequest.ForDef(TKKN_NPS.ThingDefOf.TKKN_ColdSpring), PathEndMode.Touch, TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false), -1f, null, null, 0, -1, false, RegionType.Set_Passable, false);
+				if (thing != null)
+				{
+					__result = new Job(JobDefOf.GotoSafeTemperature, thing.Position);
+					return;
+				}
+
+			}
+		}
+	}
+
+}
---
+++ Source/NPS/NPS/JobGivers/JobGiverSprings.cs
@@ -0,0 +1,49 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+// NOTE: The job that puts pawns in springs when they're too hot is in harmonypatches/jobgiverspringspatch.cs
+
+namespace TKKN_NPS
+{
+	public class JobDriver_RelaxInSpring : JobDriver_VisitJoyThing
+	{
+		private Thing SpringThing
+		{
+			get
+			{
+				return this.job.GetTarget(TargetIndex.A).Thing;
+			}
+		}
+
+
+		public override bool TryMakePreToilReservations()
+		{
+			return true;
+		}
+
+		protected override Action GetWaitTickAction()
+		{
+			return delegate
+			{
+				Pawn pawn = this.pawn;
+				float extraJoyGainFactor = 1;
+				JoyUtility.JoyTickCheckEnd(pawn, JoyTickFullJoyAction.EndJob, extraJoyGainFactor);
+			};
+		}
+
+		public override string GetReport()
+		{
+			TerrainDef terrain = this.pawn.Position.GetTerrain(this.Map);
+			if (terrain.defName == "TKKN_HotSpringsWater")
+			{
+				return "TKKN_NPS_RelaxHotSpring".Translate();
+
+			}
+			return "TKKN_NPS_RelaxColdSpring".Translate();
+		}
+	}
+}
