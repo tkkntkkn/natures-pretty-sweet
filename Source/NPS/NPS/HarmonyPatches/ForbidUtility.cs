86814119e0881ccc81323cbe09bf5a224b934b76
tkkntkkn <knordsiek@gmail.com>
2018-11-29 15:06:43 -0500

    Making it so animals will stay out of deep water?

--- Source/NPS/NPS/HarmonyPatches/ForbidUtility.cs
+++ Source/NPS/NPS/HarmonyPatches/ForbidUtility.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 using Verse;
+using Verse.AI;
 using Harmony;
 using RimWorld;
 
 namespace TKKN_NPS
 {
 
-	[HarmonyPatch(typeof(ForbidUtility), "IsForbidden", new Type[] { typeof(Thing), typeof(Pawn) })]
+	[HarmonyPatch(typeof(ForbidUtility), "IsForbidden", new Type[] { typeof(IntVec3), typeof(Pawn) })]
 	class PatchIsForbidden
 	{
 		[HarmonyPostfix]
-		public static void Postfix(Thing t, Pawn pawn, bool __result)
+		public static void Postfix(IntVec3 c, Pawn pawn, bool __result)
 		{
-			if (__result == false || pawn.Map == null)
+			if (__result == true || pawn.Map == null || !pawn.RaceProps.Animal)
 			{
 				return;
 			}
-			TerrainDef terrain = pawn.Position.GetTerrain(pawn.Map);
-			if (terrain != null && (terrain.HasTag("Lava") || terrain.HasTag("TKKN_Swim")) && !pawn.RaceProps.Humanlike) {
-				__result = false;
 
+			TerrainDef terrain = c.GetTerrain(pawn.Map);
+			if (terrain != null && (terrain.HasTag("TKKN_Lava") || terrain.HasTag("TKKN_Swim")))
+			{
+				__result = true;
 			}
 		}
 
 	}
-
 }
