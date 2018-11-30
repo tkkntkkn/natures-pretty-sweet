9710b8346c54f45f25417e907892b26f9f5dcc71
tkkntkkn <knordsiek@gmail.com>
2018-11-29 22:54:37 -0500

    working on preventing peeps from entering over water

--- Source/NPS/NPS/HarmonyPatches/CellFinder.cs
+++ Source/NPS/NPS/HarmonyPatches/CellFinder.cs
@@ -12,10 +12,15 @@ namespace TKKN_NPS
 		[HarmonyPrefix]
 		public static bool Prefix(IntVec3 root, Map map, int radius, out IntVec3 result, Predicate<IntVec3> extraValidator, bool __result)
 		{
-			///Predicate<IntVec3> validator, Map map, float roadChance, out IntVec3 result
+
 			// don't enter on deep water
-			__result = CellFinder.TryFindRandomReachableCellNear(root, map, (float)radius, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), (Predicate<IntVec3>)((IntVec3 c) => c.Standable(map) && !c.GetTerrain(map).HasTag("TKKN_Swim")  && (extraValidator == null || extraValidator(c))), (Predicate<Region>)null, out result, 999999);		
-			return false;
+			__result = CellFinder.TryFindRandomReachableCellNear(root, map, (float)radius, TraverseParms.For(TraverseMode.NoPassClosedDoors, Danger.Deadly, false), (Predicate<IntVec3>)((IntVec3 c) => c.Standable(map) && !c.GetTerrain(map).HasTag("TKKN_Lava") && !c.GetTerrain(map).HasTag("TKKN_Swim")  && (extraValidator == null || extraValidator(c))), (Predicate<Region>)null, out result, 999999);
+//			Log.Warning("result " + result.ToString());
+			return true;
 		}
 	}
+
+
+
+	
 }
--- Source/NPS/NPS/HarmonyPatches/DropCellFinder.cs
+++ Source/NPS/NPS/HarmonyPatches/DropCellFinder.cs
@@ -7,17 +7,39 @@ using Verse;
 namespace TKKN_NPS
 {
 	[HarmonyPatch(typeof(DropCellFinder))]
-	[HarmonyPatch("DropCellFinder")]
-	public static class PatchDropCellFinder
+	[HarmonyPatch("RandomDropSpot")]
+	public static class PatchRandomDropSpot
 	{
 
 		[HarmonyPrefix]
 		public static bool Prefix(Map map, IntVec3 __result)
 		{
+			Log.Warning("RandomDropSpot doing");
 			///Predicate<IntVec3> validator, Map map, float roadChance, out IntVec3 result
 			// don't drop on deep water
-			__result = CellFinderLoose.RandomCellWith((IntVec3 c) => c.Standable(map) && !c.Roofed(map) && !c.Fogged(map) && !c.GetTerrain(map).HasTag("TKKN_Swim"), map, 1000);
-			return false;
+			__result = CellFinderLoose.RandomCellWith((IntVec3 c) => c.Standable(map) && !c.GetTerrain(map).HasTag("TKKN_Swim") && !c.Roofed(map) && !c.Fogged(map), map, 1000);
+			return true;
 		}
 	}
+
+	[HarmonyPatch(typeof(DropCellFinder))]
+	[HarmonyPatch("CanPhysicallyDropInto")]
+	public static class PatchCanPhysicallyDropInto
+	{
+
+		[HarmonyPrefix]
+		public static bool Prefix(IntVec3 c, Map map, bool canRoofPunch, bool __result)
+		{
+			///Predicate<IntVec3> validator, Map map, float roadChance, out IntVec3 result
+			// don't drop on deep water
+			if (c.GetTerrain(map).HasTag("TKKN_Swim"))
+			{
+				__result = false;
+				return false;
+			}
+			return true;
+		}
+	}
+
+	
 }
